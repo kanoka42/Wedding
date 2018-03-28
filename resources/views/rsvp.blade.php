a3bd15d6eef8500ace82b163f4faa10eeeb1c335
Destry Amiott <destry.amiott@gmail.com>
2018-03-28 15:45:12 -0500

    Update rsvp.blade.php

--- resources/views/rsvp.blade.php
+++ resources/views/rsvp.blade.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
     <title>The Amiott's</title>
+   
+    
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
